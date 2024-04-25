874dab7ccaf4910567b98260b3dc9b8fd2dd890b
Raiiiden <Raidengs24@gmail.com>
2024-04-24 18:57:58 -0700

    Attempt to make scoreboard work

--- src/main/java/net/raiiiden/lvlstatsgui/procedures/UpdateScoreboardLabelProcedure.java
+++ src/main/java/net/raiiiden/lvlstatsgui/procedures/UpdateScoreboardLabelProcedure.java
@@ -1,25 +1,28 @@
 package net.raiiiden.lvlstatsgui.procedures;
 
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.scores.Scoreboard;
 import net.minecraft.world.scores.Objective;
+import net.minecraft.world.scores.Score;
 import net.minecraft.world.entity.Entity;
+import java.text.DecimalFormat;
 
 public class UpdateScoreboardLabelProcedure {
 	public static String execute(Entity entity) {
-		if (entity == null)
+		if (entity == null || !(entity instanceof Player))
 			return "";
-		String scoreboardName = "";
-		double score = 0;
-		scoreboardName = "statsAvailable";
-		score = new Object() {
-			public int getScore(String score, Entity _ent) {
-				Scoreboard _sc = _ent.level().getScoreboard();
-				Objective _so = _sc.getObjective(score);
-				if (_so != null)
-					return _sc.getOrCreatePlayerScore(_ent.getScoreboardName(), _so).getScore();
-				return 0;
-			}
-		}.getScore("statsAvailable", entity);
-		return "" + new java.text.DecimalFormat("##.##").format(score);
+
+		Player player = (Player) entity;
+		Scoreboard scoreboard = player.getScoreboard();
+		Objective objective = scoreboard.getObjective("strength");
+
+		if (objective == null)
+			return "null";
+
+		Score score = scoreboard.getOrCreatePlayerScore(player.getScoreboardName(), objective);
+		int scorePoints = score.getScore();
+
+		DecimalFormat decimalFormat = new DecimalFormat("##");
+		return decimalFormat.format(scorePoints);
 	}
 }
